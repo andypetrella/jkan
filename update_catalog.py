1b808b4184edf2c45aed3c82a5e52834d492389b
Andy Petrella <andy.petrella@kensu.io>
2021-03-09 18:48:33 +0100

    update catalog

--- update_catalog.py
+++ update_catalog.py
@@ -197,10 +197,13 @@ def main():
     dam = DAM()
     cache = Cache()
     def run():
-        print("Running at " + str(time.ctime()))
-        work(dam, cache)
-        print("Done at " + str(time.ctime()))
-        period_in_seconds = 10
+        try:
+            print("Running at " + str(time.ctime()))
+            work(dam, cache)
+            print("Done at " + str(time.ctime()))
+            period_in_seconds = 10
+        except(e):
+            pass
         threading.Timer(period_in_seconds, run).start()
     run()
 
